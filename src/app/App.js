59eadabbd8bc2e175017eb5d56da967a53e71276
תום <Tom@twm-mq.local>
2020-09-05 13:28:09 +0300

    router fix

--- src/app/App.js
+++ src/app/App.js
@@ -5,8 +5,9 @@ import Home from '../pages/home';
 import NetPayCalculator from '../pages/net-pay-calculators';
 
 function App() {
+	console.log('process.env.public_url', process.env.public_url);
 	return (
-		<Router basename={process.env.public_url}>
+		<Router basename="/financial-management">
 			<Switch>
 				<Route path="/" exact component={Home} />
 				<Route
